6badfabb07e5163385a417c4d7050343c2ebfa30
Emmett Fitzharris <emmett.fitzharris@mycit.ie>
2023-12-22 18:04:52 +0000

    Working Prediction in app

--- app.py
+++ app.py
@@ -16,22 +16,37 @@ def hello_world():  # The front page
 
 
 
-@app.route('/sumptomsList', methods=['GET'])
+@app.route('/sumptomsList', methods=['POST'])
 def get_symptom_list_API():
     symptomLabels = ml_model.get_symptom_labels(ml_model.one_hot_encoded_df_cleaned)
     return jsonify({symptomLabels})
 
 
-@app.route('/predictAI', methods=['GET'])
-# test URL http://127.0.0.1:5000/predictAI?symptom=redness_of_eyes&symptom=restlessness&symptom=runny_nose
-# above should show "common cold"
+@app.route('/predictAI', methods=['POST'])
 def predict_disease_API():
-    symptoms_list = request.args.getlist('symptom')
-    prediction = ml_model.predict_disease(symptoms_list)
-    print(prediction)
-    return prediction
+    print("Headers:", request.headers)
+    print("Raw Data:", request.data)
+    print("Data Type:", type(request.data))
 
-@app.route('/hello', methods=['GET']) # An API function
+    data = request.get_json(force=True)  # Force JSON parsing regardless of content-type header
+    print("Parsed Data:", data)
+    print("Parsed Data Type:", type(data))
+
+    if not data or 'symptomsList' not in data:
+        return jsonify({'error': 'Invalid data format'}), 400
+
+    symptoms_list = data['symptomsList']
+
+    # rest of your code
+
+    if not symptoms_list:
+        return jsonify({'error': 'Symptoms list is empty'}), 400
+
+    prediction = ml_model.predict_disease(symptoms_list)
+    return jsonify({'prediction': prediction})
+
+
+@app.route('/hello', methods=['POST']) # An API function
 def hello_world_API():
     return jsonify({'message': 'Hello, World!'})
 
